8933cd64e217eb19314b5da9116eb730f0f17446
Nicolas <nicolas.tonassi@alunoescolassociais.org.br>
2024-07-04 07:35:46 -0300

    Alteração de códigos no arquivo js

--- controler.js
+++ controler.js
@@ -2,4 +2,10 @@
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
+
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos');
+    }else{
+        alert('Campos preenchidos com sucesso');
+    }
 }
