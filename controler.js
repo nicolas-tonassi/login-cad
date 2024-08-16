d8c4a66c3c67cd50ab22d114ea6c148280e569ee
nicolas-tonassi <nicolas.tonassi@alunoescolassociais.org.br>
2024-08-16 07:35:07 -0300

    Implementação no javascript da nova função na vçidação de nome e email

--- controler.js
+++ controler.js
@@ -1,9 +1,7 @@
 // FUNÇÃO PARA VALIDAÇÃO DE ACESSO
- 
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
- 
     if(!loginEmail || !loginSenha){
         alert("Favor preencher todos os campos");
     }else{
@@ -12,36 +10,49 @@ function acessar(){
     }
 }
  
-// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+// FUNÇÃO PARA VALIDAR EMAIL
+function validarEmail(email) {
+    // Expressão regular para validar o formato do email
+    var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return re.test(String(email).toLowerCase());
+}
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES E EMAILS EM ARRAY
 var dadosLista = [];
- 
 function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
-    if(nomeUser){
-        dadosLista.push(nomeUser);
-        //console.log(dadosLista);
-        criaLista();
-        document.getElementById('nomeUser').value = "";
+    let emailUser = document.getElementById('emailUser').value;
+    
+    if(nomeUser && emailUser){
+        if (validarEmail(emailUser)) {
+            dadosLista.push({ nome: nomeUser, email: emailUser });
+            criaLista();
+            document.getElementById('nomeUser').value = "";
+            document.getElementById('emailUser').value = "";
+        } else {
+            alert("Por favor, insira um e-mail válido.");
+        }
     }else{
-        alert("Favor informe um nome para cadastro");
-    }
-}
-// FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
-function criaLista(){
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    for(let i=0;i<=(dadosLista.length - 1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-        document.getElementById('tabela').innerHTML = tabela;
+        alert("Favor informar um nome e um e-mail para cadastro");
     }
 }
  
-// FUNÇÃO PARA EDITAR NOMES DE LISTA
-function editar(i){
-    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1)], 1);
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
+    for(let i=0; i<dadosLista.length; i++){
+        tabela += "<tr><td>" + dadosLista[i].nome + "</td><td>" + dadosLista[i].email + "</td><td><button type='button' onclick='editar("+ i +")'>Editar</button><button type='button' onclick='excluir("+ i +")'>Excluir</button></td></tr>";
+    }
+    document.getElementById('tabela').innerHTML = tabela;
 }
-// FUNÇÃO PARA EXCLUIR NOME DE LISTA
+// FUNÇÃO PARA EDITAR NOMES E EMAILS NA LISTA
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[i].nome;
+    document.getElementById('emailUser').value = dadosLista[i].email;
+    excluir(i);
+}
+ 
+// FUNÇÃO PARA EXCLUIR NOME E EMAIL DA LISTA
 function excluir(i){
-    dadosLista.splice((i - 1), 1);
-    document.getElementById('tabela').deleteRow(i);
+    dadosLista.splice(i, 1);
+    criaLista();
 }
